2ffa52c65488d506e6fb980fe304b2f60eaec1dd
Mahajan Chirayu Shrikant (IFIN DES TCP EDS) <ChirayuShrikant.Mahajan@infineon.com>
2019-12-29 12:51:36 +0530

    Updated routing for comingsoon and showing movies

--- src/Routing.js
+++ src/Routing.js
@@ -5,6 +5,8 @@ import Home from "./components/Home/Home";
 import Movies from "./components/Movies/Movies";
 import Events from "./components/Events/Events";
 import { BrowserRouter, Route } from "react-router-dom";
+import Showingnow from "./components/Movies/Showingnow";
+import Comingsoon from "./components/Movies/Comingsoon";
 
 const Routing = () => {
   return (
@@ -13,6 +15,8 @@ const Routing = () => {
         <Header />
         <Route exact path="/" component={Home}></Route>
         <Route path="/movies" component={Movies}></Route>
+        <Route path="/movies/showingnow" component={Showingnow}></Route>
+        <Route path="/movies/comingsoon" component={Comingsoon}></Route>
         <Route path="/events" component={Events}></Route>
         <Footer />
       </div>
