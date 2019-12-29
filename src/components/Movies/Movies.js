e40fce51a3140665deb5f8a674fc0c894eee38b7
Mahajan Chirayu Shrikant (IFIN DES TCP EDS) <ChirayuShrikant.Mahajan@infineon.com>
2019-12-29 12:53:32 +0530

    updated

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,33 +1,26 @@
 import React, { Component } from "react";
-import { getMovies } from "../../actions";
-import PropTypes from "prop-types";
-import DisplayList from "../DisplayList/DisplayList";
-import { connect } from "react-redux";
+
 import "./Movies.css";
+import { Link } from "react-router-dom";
 
 class Movies extends Component {
-  componentDidMount() {
-    this.props.dispatch(getMovies());
-  }
   render() {
     return (
       <div className="container">
-        <h3>Movies</h3>
-        <div className="row">
-          <DisplayList moviedata={this.props.movieData} />
+        <div style={{ padding: "1rem" }}>
+          <span>
+            <button className="btn btn-info">
+              {" "}
+              <Link to="/movies/showingnow">Showing Now</Link>{" "}
+            </button>
+            <button className="btn btn-info">
+              <Link to="/movies/comingsoon">Coming Soon</Link>{" "}
+            </button>
+          </span>
         </div>
       </div>
     );
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    movieData: state.movie
-  };
-}
-Movies.protoTypes = {
-  dispatch: PropTypes.func
-};
-
-export default connect(mapStateToProps)(Movies);
+export default Movies;
